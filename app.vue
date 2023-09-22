f44b3f9f8e71be5b5aeb9954685efc705ef0635a
matotubol <matotubol@gmail.com>
2023-09-22 16:53:25 +0200

    Add all untracked files

---
+++ app.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <!-- Navigation -->
+    <nav>
+      <NuxtLink to="/">Portfolio</NuxtLink> |
+      <NuxtLink to="/about">Wie ben ik</NuxtLink> |
+      <NuxtLink to="/contact">Contact</NuxtLink>
+    </nav>
+    
+    <!-- Page Content -->
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+@font-face {
+        font-family: "Caudex";
+        src: url("/fonts/Caudex-Regular.ttf") format("woff2");
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #F9E7C7;
+  font-family: Caudex, sans-serif;
+}
+
+p {
+  margin-bottom: 20px;
+  margin-left: 10px;
+}
+/* Add some basic styling for the navigation links */
+nav {
+  margin-bottom: 20px;
+}
+
+nav a {
+  margin-right: 10px;
+  text-decoration: none;
+  color: #333;
+}
+
+nav a:hover {
+  text-decoration: underline;
+}
+.container {
+  width: calc(100% - 1000px); /* Subtracting 600px for the 300px gap on both sides */
+  margin: 0 auto; /* Horizontally center the container */
+  background-color: #8cffa0;
+}
+@media (max-width: 768px) {
+  .container {
+    gap: 0;
+    padding: 10px 300px;
+  }
+}
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+      <div class="section">
+        <p class="about-title">IK BEN EEN ILLUSTRATOR</p>
+        <p class="section-text">
+            Ik werk vanuit een gevoel van afzonderlijkheid van de wereld waarin ik leef. Er ontbreekt mij hier aan wonder. Door mijn illustraties wil ik de wereld rijker maken aan de wonderen.
+        </p>
+        <p class="section-text">
+            We leven in een super materialistische wereld en de mystiek van het bestaan wordt ons naar mijn mening ontnomen. Graag wil ik mijn bovenstaande woorden toelichten. Tegenwoordig lijkt het erop dat wij als mensen overal een antwoord op hebben, de wetenschap heeft verklaring voor alles wat er in de wereld gebeurd.
+        </p>
+        <p class="section-text">
+            Het gevoel van verwondering, dat is wat ik in mijn werkproces nastreef. Ik ben er van overtuigt dat verwondering goed is voor de menselijke ziel. Verwondering brengt ons terug in de tijd, wanneer we nog kleine kinderen waren en de wereld nog niet zo goed snapte - een staat van onschuld en niet weten- precies het tegenovergestelde toestand van de wetenschappelijke geest.Dit gevoel roep ik op door middel van creëren van onalledaagse en geestige illustraties. Onalledaags omdat je in mijn werk weinig dagelijkste taferelen zult aantreffen, en geestig omdat mijn werk veelal lach op je gezicht zult toveren.
+        </p>
+        </div>
+    </div>
+  </template>
+      <style>
+    
+      .section {
+        display: flex;
+        background-color: black;
+        align-items: center;
+        justify-content: space-evenly;
+      }
+      .about-title {
+          color:#FF4040;
+          font-size: 20px;
+          letter-spacing: 0.3rem;
+          font-weight: bold;
+      }
+
+      .section-text {
+        font-size: 14px;
+        line-height: normal;
+        text-align: justify;
+      }
+  </style>
+  

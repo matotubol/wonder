9df4b84e2dbd9e680c71c5aeaa72062213c827e3
matotubol <matotubol@gmail.com>
2023-09-24 15:56:21 +0200

    more responsive

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex-container">
-    <p class="flex-item"><span class="about-title">IK BEN EEN ILLUSTRATOR</span>
+    <div class="flex-item"><span class="about-title">IK BEN EEN ILLUSTRATOR</span>
 <p>
 Ik werk vanuit een gevoel van afzonderlijkheid van de wereld waarin ik leef. Er ontbreekt mij hier aan wonder. Door mijn illustraties wil ik de wereld rijker maken aan de wonderen.
 </p>
@@ -13,9 +13,9 @@ We leven in een super materialistische wereld en de mystiek van het bestaan word
 Het gevoel van verwondering, dat is wat ik in mijn werkproces nastreef. Ik ben er van overtuigt dat verwondering goed is voor de menselijke ziel. Verwondering brengt ons terug in de tijd, wanneer we nog kleine kinderen waren en de wereld nog niet zo goed snapte - een staat van onschuld en niet weten- precies het tegenovergestelde toestand van de wetenschappelijke geest.
 
 Dit gevoel roep ik op door middel van creëren van onalledaagse en geestige illustraties. Onalledaags omdat je in mijn werk weinig dagelijkste taferelen zult aantreffen, en geestig omdat mijn werk veelal lach op je gezicht zult toveren.
-    </p>
+</div>
     <div class="flex-item">
-    <img class="img-margin" src="/img/ikdiemaak.webp" style="width: 410px; height: 287px; object-fit: cover;" width="383" height="287" fetchpriority="high">
+    <img class="img-margin" src="/img/ikdiemaak.webp">
     <p>Ik ben een illustrator die voornamelijk werkt met grafiet en aquarel.
 Ik combineer deze twee mediums aan de hand van wat ik wil vertellen. Met waterverf illustreer ik intuïtieve en mysterieuze taferelen, met grafietpotlood kan ik mijn intentie volledig richten op detail en mezelf verdwalen in de maakproces. Het gelaat heeft een belangrijke rol in mijn kunst om de intentie van verwondering te overbrengen. Het maakproces zelf is voor mij een spirituele belevenis. Intentie die in de tekening wordt gestopt is erg belangrijk.
 Natuurlijk lukt dit niet altijd, maar het is wel mijn streven.
@@ -60,9 +60,32 @@ Natuurlijk lukt dit niet altijd, maar het is wel mijn streven.
   margin-bottom: 45px;
 }
 
-@media (max-width: 480px) {
+@media (max-width: 400px) {
   .flex-container {
     flex-direction: column; /* Stack flex items on smaller screens */
+    font-size: 16px;
+  }
+}
+@media (width <= 930px)
+{
+  .about-title {
+    font-size: 18px;
+    white-space: nowrap;
+  }
+  .flex-container {
+    color: #241800;
+    text-align: left;
+    gap:10px;
+  }
+}
+@media (width < 768px) {
+  .flex-container {
+    font-size: 18px;
+    flex-direction: column; /* Stack flex items on smaller screens */
+    gap: 10px;
+  }
+  .flex-item {
+    max-width: 100%;
   }
 }
 </style>
